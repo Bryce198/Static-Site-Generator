73a34ab57f2ec11e4b6739f6b41c50fb1ce9050a
Bryce <bryce@brycemacneo.tail055702.ts.net>
2026-09-04 17:17:44 -0400

    D: Added LeafNode class inherited from the HTMLNode class

--- src/static_site_generator/htmlnode.py
+++ src/static_site_generator/htmlnode.py
@@ -6,7 +6,7 @@ class HTMLNode():
                  props: dict[str, str] | None = None):
         self.tag = tag
         self.value = value
-        self.children = children or []
+        self.__children = children or []
         self.props = props or {}
 
     def to_html(self):
@@ -18,4 +18,34 @@ class HTMLNode():
         return " ".join(f"{k}={v}" for k, v in self.props.items())
 
     def __repr__(self) -> str:
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.__children}, props={self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, 
+                 tag: str | None, 
+                 value: str, 
+                 props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+        self.tag = tag
+        self.value = value
+        self.props = props or {}
+
+    def to_html(self):
+        attributes = ""
+        if self.value is None:
+            raise ValueError("Value is empty")
+        if self.tag is None:
+            return str(self.value)
+        else:
+            if not self.props:
+                return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+        for k, v in self.props.items():
+            attributes += f" {k}={v}"
+        return f'<{self.tag}{attributes}>{self.value}</{self.tag}>'
+
+    def __repr__(self) -> str:
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+
